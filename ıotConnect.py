054a895e463d09af2877f6c2a2b76f1982289228
yusuftogtay <yusuf.togtay@gmail.com>
2020-01-24 20:34:14 +0300

    **Create Iot Class **Create initializer method **Create setSubscribe method **Create loop method **Create setTopic method

--- ıotConnect.py
+++ ıotConnect.py
@@ -1,32 +1,71 @@
 import paho.mqtt.client as mqtt
-import configure
-
-host = configure.mqttHost()
-port = configure.mqttPort()
-keepalive = configure.mqttKeepAlive()
+import configure as conf
+from device import Device
 
 
-def on_subscribe():
-    pass
-def on_connect():
-    pass
-def on_message():
-    pass
-def on_log():
-    pass
-def on_publish():
-    pass
-def on_disconnect():
-    pass
-def on_unsubscribe():
-    pass
+class Iot:
+    def __init__(self):
+        self.host = conf.getMqttHost()
+        self.port = conf.getMqttPort()
+        self.keepalive = conf.getMqttKeepAlive()
+        self.mqttClient = mqtt.Client("SIS_ADMIN")
+        self.mqttClient.connect(host=self.host, port=self.port, keepalive=self.keepalive)
+        self.mqttClient.on_message = self.on_message
+        self.mqttClient.on_connect = self.on_connect
+        self.mqttClient.on_log = self.on_log
+        self.mqttClient.on_publish = self.on_publish
+        self.mqttClient.on_subscribe = self.on_subscribe
+        self.mqttClient.on_disconnect = self.on_disconnect
+        self.mqttClient.on_unsubscribe = self.on_unsubscribe
+        self.device = Device()
+        self.rc = 0
+        self.deviceList = list()
 
-mqttClient = mqtt.Client("SIS_ADMIN")
-mqttClient.connect(host=host, port=port, keepalive=keepalive)
-mqttClient.on_message = on_message
-mqttClient.on_connect = on_connect
-mqttClient.on_log = on_log
-mqttClient.on_publish = on_publish
-mqttClient.on_subscribe = on_subscribe
-mqttClient.on_disconnect = on_disconnect
-mqttClient.on_unsubscribe = on_unsubscribe
+    def setSubscribe(self):
+        subs = self.setTopic()
+        for sub in subs:
+            self.mqttClient.subscribe(sub)
+
+    def loop(self):
+        while self.rc == 0:
+            self.rc = self.mqttClient.loop()
+        print("RC: " + str(self.rc))
+
+    def setTopic(self):
+        activeHubs = self.device.getActivateHub()
+        for hub in activeHubs:
+            activeChildSensor = self.device.getActivateChildSensor(activehubs=hub)
+            db = connect()
+            for data in conf.getTopics():
+                topic = hub + "/" + data
+                self.deviceList.append(topic)
+            for child in activeChildSensor:
+                for data in conf.getTopics():
+                    topic = hub + "/" + child + "/" + data
+                    self.deviceList.append(topic)
+        return self.deviceList
+
+
+
+
+
+    def on_subscribe(self):
+        pass
+
+    def on_connect(self):
+        pass
+
+    def on_message(self):
+        pass
+
+    def on_log(self):
+        pass
+
+    def on_publish(self):
+        pass
+
+    def on_disconnect(self):
+        pass
+
+    def on_unsubscribe(self):
+        pass
