81fefe8eef5d5c03fa5166293d50b39f555f5896
yusuftogtay <yusuf.togtay@gmail.com>
2020-01-24 20:22:15 +0300

    **remove print function **add topics data read function

--- configure.py
+++ configure.py
@@ -3,94 +3,90 @@ import json
 with open("package.json") as jsonFile:
     data = json.load(jsonFile)
 
-def mqttHost():
+def getMqttHost():
     """
     :return: <list>
     """
     host = data["connect"]["mqtt"]["MQTT_HOST"]
-    print("MQTT Host " + host)
     return host
 
 
-def mqttPort():
+def getMqttPort():
     """
         :return: <list>
     """
     port = data["connect"]["mqtt"]["MQTT_PORT"]
-    print("MQTT Port: " + port)
     port = int(port)
     return port
 
 
-def mqttUserName():
+def getMqttUserName():
     """
         :return: <list>
     """
     userName = data["connect"]["mqtt"]["MQTT_USERNAME"]
-    print("MQTT Username: " + userName)
     return userName
 
 
-def mqttPasswd():
+def getMqttPasswd():
     """
         :return: <list>
     """
     passwd = data["connect"]["mqtt"]["MQTT_PASSWORD"]
-    print("MQTT Password: " + passwd)
     return passwd
 
 
-def mqttKeepAlive():
+def getMqttKeepAlive():
     """
         :return: <list>
     """
     keepalive = data["connect"]["mqtt"]["MQTT_KEEPALIVE"]
-    print("MQTT Keepalive: ")
     keep = int(keepalive)
     return  keep
 
 
-def mysqlHost():
+def getMysqlHost():
     """
         :return: <list>
     """
     host = data["connect"]["mysql"]["MYSQL_HOST"]
-    print("MYSQL Host: " + host)
     return host
 
 
-def mysqlPort():
+def getMysqlPort():
     """
         :return: <list>
     """
     port = data["connect"]["mysql"]["MYSQL_PORT"]
-    print("MYSQL port: " + port)
     return port
 
 
-def mysqlUserName():
+def getMysqlUserName():
     """
         :return: <list>
     """
     userName = data["connect"]["mysql"]["MYSQL_USERNAME"]
-    print("MYSQL Username: " + userName)
     return userName
 
 
-def mysqlPasswd():
+def getMysqlPassword():
     """
         :return: <list>
     """
-    passwd = data["connect"]["mysql"]["MYSQL_PASSWORD"]
-    print("MYSQL Password: " + passwd)
-    return passwd
+    password = data["connect"]["mysql"]["MYSQL_PASSWORD"]
+    return password
 
 
-def mysqlDb():
+def getMysqlDb():
     """
         :return: <list>
     """
     keepalive = data["connect"]["mysql"]["MYSQL_DATABASE"]
-    print("MYSQL Database: " + keepalive)
     return keepalive
 
+def getTopics():
+    top1 = data["topics"]["data"]
+    top2 = data["topics"]["status"]
+    top3 = data["topics"]["info"]
+    topics = (top1,top2,top3)
+    return topics
