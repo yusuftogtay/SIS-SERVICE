d99cc5bc5a7bc8947aa1fb0c00d52334bb10e4ef
yusuftogtay <yusuf.togtay@gmail.com>
2020-01-26 00:24:34 +0300

    **Remove try except, **Create getDeviceType, **Remove insertWsSensorData method, **Remove insertSmSensorData method, **Create insertIotMessage method

--- dbConnect.py
+++ dbConnect.py
@@ -16,15 +16,13 @@ class Connect(object):
             database=self.mysqlDb,
             port=self.mysqlPort
         )
-        try:
-            if self.db.is_connected():
-                print("Connection successful")
-                print("Connected server:" + conf.mysqlHost())
-                print("Connected Port: " + conf.mysqlPort())
-                print("Connected Database: " + conf.mysqlDb())
-        except:
-            print("Connection failed")
-            self.db.ping(reconnect=True, attempts=1, delay=5)
+        if self.db.is_connected():
+            print("Connection successful")
+            print("Connected server:" + conf.getMysqlHost())
+            print("Connected Port: " + conf.getMysqlPort())
+            print("Connected Database: " + conf.getMysqlDb())
+        else:
+            self.db.ping(reconnect= True, attempts=1,delay=5)
 
     def getActiveHubList(self):
         """
@@ -32,6 +30,15 @@ class Connect(object):
         """
         self.cursor = self.db.cursor()
         self.cursor.execute("SELECT DEVICE_ID FROM DEVICE_STOCK WHERE DEVICE_TYPE = 'HUB'")
+        result = self.cursor.fetchall()
+        return result
+
+    def getDeviceType(self):
+        """
+        :return:
+        """
+        self.cursor = self.db.cursor()
+        self.cursor.execute("SELECT * FROM DEVICE_STOCK")
         result = self.cursor.fetchall()
         return result
 
@@ -46,38 +53,29 @@ class Connect(object):
         result = self.cursor.fetchall()
         return result
 
-    def insertWsSensorData(self ,**kwargs):
-        deviceID = kwargs.get('DeviceID', None)
-        airQuality = kwargs.get('airQuality', None)
-        airTemperature = kwargs.get('airTemperature', None)
-        airHumidity = kwargs.get('aitHumidity', None)
-        rain = kwargs.get('rain', None)
-        sendTime = kwargs.get('sendTime', None)
-        self.db.connect()
+    # noinspection PyGlobalUndefined
+    def insertIotMessage(self, deviceid, messagetype, values):
         cursor = self.db.cursor()
-        sql = "INSERT INTO WS_SENS_STATUS VALUES (%s,%s,%s,%s,%s,%s)"
-        values = (deviceID,airQuality,airTemperature,airHumidity,rain,sendTime)
-        cursor.execute(sql, values)
-        self.db.commit()
-        print("Inserted: ", cursor.lastrowid)
-        cursor.close()
-
-    def insertSmSensorData(self,**kwargs):
-        deviceID = kwargs.get('DeviceID', None)
-        s3 = kwargs.get('s3', None)
-        s6 = kwargs.get('s6', None)
-        s9 = kwargs.get('s9', None)
-        downTemperature = kwargs.get('downTemperature', None)
-        upTemperature = kwargs.get('upTemperature', None)
-        sendTime = kwargs.get('sendTime', None)
-        cursor = self.db.cursor()
-        sql = "INSERT INTO SM_SENS_STATUS VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        values = (deviceID, s3, s6, s9, downTemperature, upTemperature, sendTime)
-        cursor.execute(sql,values)
-        self.db.commit()
-        print("Inserted: ", cursor.lastrowid)
-        cursor.close()
-
-
-
+        deviceTypeList = self.getDeviceType()
+        global deviceType
+        for devices in deviceTypeList:
+            if devices[0] == deviceid:
+                deviceType = devices[1]
+        if deviceType == "HUB":
+            if messagetype == "Data":
+                pass
+            if messagetype == "Info":
+                pass
+            if messagetype == "Status":
+                pass
+        if messagetype == "Status":
+            if deviceType == "SM":
+                pass
+            elif deviceType == "WS":
+                pass
+        elif messagetype == "Data":
+            if deviceType == "SM":
+                pass
+            elif deviceType == "WS":
+                pass
 
